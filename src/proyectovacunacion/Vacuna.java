22fd07ed908a886afaaf795a09a1d15a111d8c6b
dsilv <dsilv@DESKTOP-MAPPD7V>
2021-05-24 19:11:24 -0300

    agregue clase vacuna

---
+++ src/proyectovacunacion/Vacuna.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectovacunacion;
+
+/**
+ *
+ * @author dsilv
+ */
+public class Vacuna {
+    private String tipo;
+    
+    
+    public Vacuna(String tipoVacuna){
+        tipo = tipoVacuna
+    }
+    
+    public String getTipoVacuna(){
+     return tipo;
+    }
+}
