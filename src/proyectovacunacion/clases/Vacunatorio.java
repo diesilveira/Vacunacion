9dabf4df1b1a5e7c44f07777203bc441d0528a12
dsilv <dsilv@DESKTOP-MAPPD7V>
2021-05-25 18:04:59 -0300

    cambios en vacunatorio

--- src/proyectovacunacion/clases/Vacunatorio.java
+++ src/proyectovacunacion/clases/Vacunatorio.java
@@ -4,37 +4,71 @@
  * and open the template in the editor.
  */
 package proyectovacunacion.clases;
+
 import proyectovacunacion.clases.Persona;
 import java.util.Queue;
+
 /**
  *
  * @author dsilv
  */
 public class Vacunatorio {
-    public String departamento; // Departamento al que pertenece
-    public String barrio;       // Barrio en que se encuentra
-    public String id; /*identificacion segun departamento barrio y numero
+
+    private String departamento; // Departamento al que pertenece
+    private String barrio;       // Barrio en que se encuentra
+    private String id;
+    /*identificacion segun departamento barrio y numero
                         Ejemplo MOTC01 -> Montevideo Tres Cruces Vacunatorio 1
                         (puede haber varios por barrio)
                         Las 2 primeras letras depto. las otras dos barrio,
                         los dos nuemros el numero de vacunatorio en ese barrio
-                        */
-    public Queue<Persona> colaDePersonas;
-    
-    public Vacunatorio(String depto, String bo, String nId){
+     */
+    private Queue<Persona> colaDePersonas;
+    private int vacunasDisponibles;
+
+    public Vacunatorio(String depto, String bo, String nId) {
         departamento = depto;
         barrio = bo;
         id = nId;
     }
-    
-    public boolean agregarPersona(Persona p){
-        try {
-            colaDePersonas.add(p);
-            return true;
-        } catch (Exception ex){
-            System.out.println("Error "+ ex.getMessage() + " al cargar  "
-                    + p.getCedula() + " al vacunatorio" + this.id);
+
+    public boolean agregarPersona(Persona p) {
+        if (vacunasDisponibles > 0) {
+            try {
+                colaDePersonas.add(p);
+                vacunasDisponibles--;
+                return true;
+            } catch (Exception ex) {
+                System.out.println("Error " + ex.getMessage() + " al cargar  "
+                        + p.getCedula() + " al vacunatorio" + this.id);
+                return false;
+            }
+        } else {
             return false;
         }
-}
+
+    }
+
+    public boolean borrarPersona(Persona p) {
+
+        try {
+            colaDePersonas.remove(p);
+            vacunasDisponibles++;
+            return true;
+        } catch (Exception ex) {
+            System.out.println("Error " + ex.getMessage() + " al borrar  "
+                    + p.getCedula() + " del vacunatorio" + this.id);
+            return false;
+        }
+
+    }
+
+    public int getVacunasDisponibles() {
+        return vacunasDisponibles;
+    }
+
+    public void setVacunasDisponibles(int vacunas) {
+        vacunasDisponibles = vacunas;
+    }
+
 }
