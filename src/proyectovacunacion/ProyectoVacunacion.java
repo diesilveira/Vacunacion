5f5f03f177d443010a68f2a851445e1abf7f3873
DanielSarasolaPioli <56884981+DanielSarasolaPioli@users.noreply.github.com>
2021-05-25 07:32:55 -0300

    Prueba.

--- src/proyectovacunacion/ProyectoVacunacion.java
+++ src/proyectovacunacion/ProyectoVacunacion.java
@@ -16,6 +16,7 @@ public class ProyectoVacunacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Prueba
     }
     
     public static void inicializar(){
